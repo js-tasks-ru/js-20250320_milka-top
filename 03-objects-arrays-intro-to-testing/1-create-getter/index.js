5e692876be8c86f023d829db9d6c7bd488977a0b
Ivan Milenin <i.milenin@lipt-soft.ru>
2025-04-11 08:10:07 +0300

    [HW-03-1] add init HW

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  let fields = path.split('.');
+  return obj => {
+    let current = obj;
+    for (let field of fields) {
+      if (!current.hasOwnProperty(field)) {
+        return undefined;
+      }
+      current = current[field];
+    }
+    return current;
+  };
 }
