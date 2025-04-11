6d9691359ab575de6078701a5cf850002f5188f6
Ivan Milenin <i.milenin@lipt-soft.ru>
2025-04-11 08:10:36 +0300

    [HW-03-3] add init HW

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,4 +6,25 @@
  */
 export function trimSymbols(string, size) {
 
+  return recursiveTrim("", 0, string, size ?? Number.MAX_SAFE_INTEGER);
+}
+
+function recursiveTrim(symbol, counter, text, size) {
+  if (text.length === 0) {
+    return "";
+  }
+  const letter = text.charAt(0);
+  let slicedText = text.slice(1);
+  if (size <= 0) {
+    return recursiveTrim(letter, 0, slicedText, size);
+  }
+
+  if (letter === symbol) {
+    if (counter < size) {
+      return letter + recursiveTrim(letter, counter + 1, slicedText, size);
+    } else {
+      return recursiveTrim(letter, counter + 1, slicedText, size);
+    }
+  }
+  return letter + recursiveTrim(letter, 1, slicedText, size);
 }
